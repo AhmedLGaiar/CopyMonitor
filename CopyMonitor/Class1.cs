0756ef97dc78557a46a3bccbd51118f410dbdd07
AhmedLGaiar <ahmedlgaiar@gmail.com>
2025-03-25 16:53:56 +0200

    Add project files.

---
+++ CopyMonitor/Class1.cs
@@ -0,0 +1,59 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace CopyMonitor
+{
+    public class CopyMonitorLevels : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            try
+            {
+                List<RevitLinkInstance> linkInstances = new FilteredElementCollector(doc)
+                                    .OfClass(typeof(RevitLinkInstance)).Cast<RevitLinkInstance>().ToList();
+                if (!linkInstances.Any())
+                {
+                    TaskDialog.Show("Error", "No linked models found.");
+                    return Result.Failed;
+                }
+                foreach (var linkInstance in linkInstances)
+                {
+                    Document linkedDoc = linkInstance.GetLinkDocument();
+                    var linkedLevels = new FilteredElementCollector(linkedDoc).OfClass(typeof(Level))
+                                                                        .Cast<Level>().ToList();
+                    if (!linkedLevels.Any())
+                    {
+                        TaskDialog.Show("Error", "No levels found in the linked model.");
+                        return Result.Failed;
+                    }
+
+                    Dictionary<string, Level> existingLevels = new FilteredElementCollector(doc)
+                                 .OfClass(typeof(Level)).Cast<Level>().ToDictionary(l => l.Name, l => l);
+
+                    using (Transaction tx = new Transaction(doc, "Copy Monitor Levels"))
+                    {
+                        tx.Start();
+
+                        foreach (var level in existingLevels)
+                        {
+
+                        }
+
+                        tx.Commit();
+                    }
+
+                }
+
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
